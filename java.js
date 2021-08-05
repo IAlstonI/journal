09e892a1e6ab726ffaf12690064c8d38288bd963
Alston Haynes <alston13@icloud.com>
2021-08-05 16:10:37 -0700

    jo

---
+++ java.js
@@ -0,0 +1,25 @@
+function addJournalEntry(create_date, entry, confidence)
+{
+      // Create a new journal object
+      let newEntry = { 
+        creation_date: create_date, 
+        journal_entry: entry,
+        confidence_level: confidence              
+ }
+
+ // Add the entry to the Array and to the DOM (u do this...)
+
+ // Call your logJournalEntries function to log all entries in the array to the console
+}
+             
+            
+      
+
+      // Add the entry to the Array and to the DOM (u do this...)
+
+      // Call your logJournalEntries function to log all entries in the array to the console
+
+
+      newEntry = prompt("How many Journal Entries do you want.");
+
+
